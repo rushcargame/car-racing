be94069ee36af52fea4e5bc2de8e20723f16dbf5
rushcargame <fahadkhatri834@gmail.com>
2026-08-04 17:39:14 +0500

    Add enemy creation and collision detection

--- script.js
+++ script.js
@@ -66,3 +66,129 @@ function drawPlayer() {
     ctx.fillRect(player.x + 8, player.y + 8, 12, 18);
     ctx.fillRect(player.x + 30, player.y + 8, 12, 18);
 }
+function createEnemy() {
+
+    const lane = Math.floor(Math.random() * 3);
+
+    enemies.push({
+        x: canvas.width / 2 - ROAD_WIDTH / 2 + lane * LANE_WIDTH + 20,
+        y: -120,
+        width: 50,
+        height: 90
+    });
+
+}
+
+setInterval(() => {
+
+    if(gameRunning){
+        createEnemy();
+    }
+
+},1200);
+
+function drawEnemies(){
+
+    ctx.fillStyle="#0095ff";
+
+    enemies.forEach(enemy=>{
+
+        ctx.fillRect(
+            enemy.x,
+            enemy.y,
+            enemy.width,
+            enemy.height
+        );
+
+    });
+
+}
+
+function updateEnemies(){
+
+    for(let i=enemies.length-1;i>=0;i--){
+
+        enemies[i].y+=speed;
+
+        if(enemies[i].y>canvas.height){
+
+            enemies.splice(i,1);
+
+            score+=10;
+
+        }
+
+    }
+
+}
+
+function updatePlayer(){
+
+    const left=canvas.width/2-ROAD_WIDTH/2;
+    const right=canvas.width/2+ROAD_WIDTH/2-player.width;
+
+    if(keys["ArrowLeft"] && player.x>left){
+        player.x-=player.speed;
+    }
+
+    if(keys["ArrowRight"] && player.x<right){
+        player.x+=player.speed;
+    }
+
+}
+function checkCollision(){
+
+    for(let enemy of enemies){
+
+        if(
+            player.x < enemy.x + enemy.width &&
+            player.x + player.width > enemy.x &&
+            player.y < enemy.y + enemy.height &&
+            player.y + player.height > enemy.y
+        ){
+
+            gameRunning = false;
+
+            alert("💥 GAME OVER\n\nScore : " + score);
+
+            location.reload();
+
+        }
+
+    }
+
+}
+
+function gameLoop(){
+
+    if(!gameRunning) return;
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    drawRoad();
+
+    updatePlayer();
+
+    updateEnemies();
+
+    drawEnemies();
+
+    drawPlayer();
+
+    checkCollision();
+
+    document.getElementById("score").innerText = score;
+
+    requestAnimationFrame(gameLoop);
+
+}
+
+window.addEventListener("resize",()=>{
+
+    canvas.width = window.innerWidth;
+
+    canvas.height = window.innerHeight;
+
+    player.y = canvas.height - 130;
+
+});
